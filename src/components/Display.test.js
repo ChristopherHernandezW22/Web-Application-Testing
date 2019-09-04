6c5844b1411d689ddd2ef37f12aabb87ff5c78d7
Christopher Hernandez <cerberean661@gmail.com>
2019-09-04 15:30:57 -0700

    further testing with yarn test

---
+++ src/components/Display.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Display from './Display.js';
+
+describe('<Display />', () => {
+    it('renders without crashing', () => {
+        render(<Display />);
+    });
+    it('props working', () => {
+        const display = render(<Display strikes={2} balls={3} />);
+        display.getByText(/Strikes: 2/);
+        display.getByText(/Balls: 3/);
+
+    });
+});
