e2e253f657872b1b3264ec9710f5439d2a028e24
Christopher Hernandez <cerberean661@gmail.com>
2019-09-04 23:29:13 -0700

    buttons functional in App.js. Needs adjustments

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Display from './components/Display.js';
 import Controls from'./components/Controls.js';
 
 function App() {
+  const [strikes, setStrikes] = useState(0);
+  const [balls, setBalls] = useState(0);
+  const scoreStrikes = () => {
+    setStrikes(strikes+1)
+  };
+  const scoreBalls = () => {
+    setBalls(balls+1)
+  };
+  const scoreFoul = () => {
+    setStrikes(strikes+1)
+  };
+  const scoreHit = () => {
+    setStrikes(0);
+    setBalls(0);
+  };
   return (
     <div className="App">
-      <Display strikes={2} balls={3} />
-      <Controls strike={() => console.log('strike')}/>
+      <Display strikes={strikes} balls={balls} />
+      <Controls strike={scoreStrikes}
+                ball={scoreBalls}
+                foul={scoreFoul}
+                hit={scoreHit}
+                />
     </div>
   );
 }
